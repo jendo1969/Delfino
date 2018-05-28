51eb060598ac7373e6f7026b5bcd7a6067eb9592
jendo1969 <bzq00764@nifty.ne.jp>
2018-05-28 22:00:57 +0900

    20180528

---
+++ sci_echoback_cpu01/CPU1_RAM/subdir_vars.mk
@@ -0,0 +1,82 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+ASM_SRCS += \
+D:/ti/controlSUITE/device_support/F2837xS/v180/F2837xS_common/source/F2837xS_CodeStartBranch.asm \
+D:/ti/controlSUITE/device_support/F2837xS/v180/F2837xS_common/source/F2837xS_usDelay.asm 
+
+C_SRCS += \
+D:/ti/controlSUITE/device_support/F2837xS/v180/F2837xS_examples_Cpu1/sci_echoback/cpu01/Example_2837xSSci_Echoback.c \
+D:/ti/controlSUITE/device_support/F2837xS/v180/F2837xS_common/source/F2837xS_DefaultISR.c \
+D:/ti/controlSUITE/device_support/F2837xS/v180/F2837xS_headers/source/F2837xS_GlobalVariableDefs.c \
+D:/ti/controlSUITE/device_support/F2837xS/v180/F2837xS_common/source/F2837xS_Gpio.c \
+D:/ti/controlSUITE/device_support/F2837xS/v180/F2837xS_common/source/F2837xS_PieCtrl.c \
+D:/ti/controlSUITE/device_support/F2837xS/v180/F2837xS_common/source/F2837xS_PieVect.c \
+D:/ti/controlSUITE/device_support/F2837xS/v180/F2837xS_common/source/F2837xS_SysCtrl.c 
+
+C_DEPS += \
+./Example_2837xSSci_Echoback.d \
+./F2837xS_DefaultISR.d \
+./F2837xS_GlobalVariableDefs.d \
+./F2837xS_Gpio.d \
+./F2837xS_PieCtrl.d \
+./F2837xS_PieVect.d \
+./F2837xS_SysCtrl.d 
+
+OBJS += \
+./Example_2837xSSci_Echoback.obj \
+./F2837xS_CodeStartBranch.obj \
+./F2837xS_DefaultISR.obj \
+./F2837xS_GlobalVariableDefs.obj \
+./F2837xS_Gpio.obj \
+./F2837xS_PieCtrl.obj \
+./F2837xS_PieVect.obj \
+./F2837xS_SysCtrl.obj \
+./F2837xS_usDelay.obj 
+
+ASM_DEPS += \
+./F2837xS_CodeStartBranch.d \
+./F2837xS_usDelay.d 
+
+OBJS__QUOTED += \
+"Example_2837xSSci_Echoback.obj" \
+"F2837xS_CodeStartBranch.obj" \
+"F2837xS_DefaultISR.obj" \
+"F2837xS_GlobalVariableDefs.obj" \
+"F2837xS_Gpio.obj" \
+"F2837xS_PieCtrl.obj" \
+"F2837xS_PieVect.obj" \
+"F2837xS_SysCtrl.obj" \
+"F2837xS_usDelay.obj" 
+
+C_DEPS__QUOTED += \
+"Example_2837xSSci_Echoback.d" \
+"F2837xS_DefaultISR.d" \
+"F2837xS_GlobalVariableDefs.d" \
+"F2837xS_Gpio.d" \
+"F2837xS_PieCtrl.d" \
+"F2837xS_PieVect.d" \
+"F2837xS_SysCtrl.d" 
+
+ASM_DEPS__QUOTED += \
+"F2837xS_CodeStartBranch.d" \
+"F2837xS_usDelay.d" 
+
+C_SRCS__QUOTED += \
+"D:/ti/controlSUITE/device_support/F2837xS/v180/F2837xS_examples_Cpu1/sci_echoback/cpu01/Example_2837xSSci_Echoback.c" \
+"D:/ti/controlSUITE/device_support/F2837xS/v180/F2837xS_common/source/F2837xS_DefaultISR.c" \
+"D:/ti/controlSUITE/device_support/F2837xS/v180/F2837xS_headers/source/F2837xS_GlobalVariableDefs.c" \
+"D:/ti/controlSUITE/device_support/F2837xS/v180/F2837xS_common/source/F2837xS_Gpio.c" \
+"D:/ti/controlSUITE/device_support/F2837xS/v180/F2837xS_common/source/F2837xS_PieCtrl.c" \
+"D:/ti/controlSUITE/device_support/F2837xS/v180/F2837xS_common/source/F2837xS_PieVect.c" \
+"D:/ti/controlSUITE/device_support/F2837xS/v180/F2837xS_common/source/F2837xS_SysCtrl.c" 
+
+ASM_SRCS__QUOTED += \
+"D:/ti/controlSUITE/device_support/F2837xS/v180/F2837xS_common/source/F2837xS_CodeStartBranch.asm" \
+"D:/ti/controlSUITE/device_support/F2837xS/v180/F2837xS_common/source/F2837xS_usDelay.asm" 
+
+
