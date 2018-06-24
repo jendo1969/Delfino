06ed19f95def2982569466fe303e728d7ab0967c
jendo1969 <bzq00764@nifty.ne.jp>
2018-06-24 22:46:28 +0900

    20180624

---
+++ adc_soc_continuous_dma_cpu01/CPU1_FLASH/subdir_rules.mk
@@ -0,0 +1,85 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Each subdirectory must supply rules for building sources it contributes
+F2837xS_Adc.obj: D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_Adc.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"D:/ti/ccs700/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/bin/cl2000" -v28 -ml -mt --vcu_support=vcu2 --cla_support=cla1 --float_support=fpu32 --tmu_support=tmu0 --include_path="D:/ti/ccs700/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/include" --include_path="D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/headers/include" --include_path="D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/include" -g --define=CPU1 --define=_LAUNCHXL_F28377S --define=_FLASH --display_error_number --diag_warning=225 --diag_suppress=1311 --preproc_with_compile --preproc_dependency="F2837xS_Adc.d" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+F2837xS_CodeStartBranch.obj: D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_CodeStartBranch.asm $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"D:/ti/ccs700/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/bin/cl2000" -v28 -ml -mt --vcu_support=vcu2 --cla_support=cla1 --float_support=fpu32 --tmu_support=tmu0 --include_path="D:/ti/ccs700/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/include" --include_path="D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/headers/include" --include_path="D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/include" -g --define=CPU1 --define=_LAUNCHXL_F28377S --define=_FLASH --display_error_number --diag_warning=225 --diag_suppress=1311 --preproc_with_compile --preproc_dependency="F2837xS_CodeStartBranch.d" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+F2837xS_DefaultISR.obj: D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_DefaultISR.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"D:/ti/ccs700/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/bin/cl2000" -v28 -ml -mt --vcu_support=vcu2 --cla_support=cla1 --float_support=fpu32 --tmu_support=tmu0 --include_path="D:/ti/ccs700/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/include" --include_path="D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/headers/include" --include_path="D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/include" -g --define=CPU1 --define=_LAUNCHXL_F28377S --define=_FLASH --display_error_number --diag_warning=225 --diag_suppress=1311 --preproc_with_compile --preproc_dependency="F2837xS_DefaultISR.d" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+F2837xS_Dma.obj: D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_Dma.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"D:/ti/ccs700/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/bin/cl2000" -v28 -ml -mt --vcu_support=vcu2 --cla_support=cla1 --float_support=fpu32 --tmu_support=tmu0 --include_path="D:/ti/ccs700/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/include" --include_path="D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/headers/include" --include_path="D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/include" -g --define=CPU1 --define=_LAUNCHXL_F28377S --define=_FLASH --display_error_number --diag_warning=225 --diag_suppress=1311 --preproc_with_compile --preproc_dependency="F2837xS_Dma.d" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+F2837xS_GlobalVariableDefs.obj: D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/headers/source/F2837xS_GlobalVariableDefs.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"D:/ti/ccs700/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/bin/cl2000" -v28 -ml -mt --vcu_support=vcu2 --cla_support=cla1 --float_support=fpu32 --tmu_support=tmu0 --include_path="D:/ti/ccs700/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/include" --include_path="D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/headers/include" --include_path="D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/include" -g --define=CPU1 --define=_LAUNCHXL_F28377S --define=_FLASH --display_error_number --diag_warning=225 --diag_suppress=1311 --preproc_with_compile --preproc_dependency="F2837xS_GlobalVariableDefs.d" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+F2837xS_Gpio.obj: D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_Gpio.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"D:/ti/ccs700/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/bin/cl2000" -v28 -ml -mt --vcu_support=vcu2 --cla_support=cla1 --float_support=fpu32 --tmu_support=tmu0 --include_path="D:/ti/ccs700/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/include" --include_path="D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/headers/include" --include_path="D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/include" -g --define=CPU1 --define=_LAUNCHXL_F28377S --define=_FLASH --display_error_number --diag_warning=225 --diag_suppress=1311 --preproc_with_compile --preproc_dependency="F2837xS_Gpio.d" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+F2837xS_PieCtrl.obj: D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_PieCtrl.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"D:/ti/ccs700/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/bin/cl2000" -v28 -ml -mt --vcu_support=vcu2 --cla_support=cla1 --float_support=fpu32 --tmu_support=tmu0 --include_path="D:/ti/ccs700/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/include" --include_path="D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/headers/include" --include_path="D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/include" -g --define=CPU1 --define=_LAUNCHXL_F28377S --define=_FLASH --display_error_number --diag_warning=225 --diag_suppress=1311 --preproc_with_compile --preproc_dependency="F2837xS_PieCtrl.d" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+F2837xS_PieVect.obj: D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_PieVect.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"D:/ti/ccs700/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/bin/cl2000" -v28 -ml -mt --vcu_support=vcu2 --cla_support=cla1 --float_support=fpu32 --tmu_support=tmu0 --include_path="D:/ti/ccs700/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/include" --include_path="D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/headers/include" --include_path="D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/include" -g --define=CPU1 --define=_LAUNCHXL_F28377S --define=_FLASH --display_error_number --diag_warning=225 --diag_suppress=1311 --preproc_with_compile --preproc_dependency="F2837xS_PieVect.d" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+F2837xS_SysCtrl.obj: D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_SysCtrl.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"D:/ti/ccs700/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/bin/cl2000" -v28 -ml -mt --vcu_support=vcu2 --cla_support=cla1 --float_support=fpu32 --tmu_support=tmu0 --include_path="D:/ti/ccs700/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/include" --include_path="D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/headers/include" --include_path="D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/include" -g --define=CPU1 --define=_LAUNCHXL_F28377S --define=_FLASH --display_error_number --diag_warning=225 --diag_suppress=1311 --preproc_with_compile --preproc_dependency="F2837xS_SysCtrl.d" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+F2837xS_usDelay.obj: D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_usDelay.asm $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"D:/ti/ccs700/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/bin/cl2000" -v28 -ml -mt --vcu_support=vcu2 --cla_support=cla1 --float_support=fpu32 --tmu_support=tmu0 --include_path="D:/ti/ccs700/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/include" --include_path="D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/headers/include" --include_path="D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/include" -g --define=CPU1 --define=_LAUNCHXL_F28377S --define=_FLASH --display_error_number --diag_warning=225 --diag_suppress=1311 --preproc_with_compile --preproc_dependency="F2837xS_usDelay.d" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+adc_soc_continuous_dma_cpu01.obj: D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/examples/cpu1/adc_soc_continuous_dma/cpu01/adc_soc_continuous_dma_cpu01.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"D:/ti/ccs700/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/bin/cl2000" -v28 -ml -mt --vcu_support=vcu2 --cla_support=cla1 --float_support=fpu32 --tmu_support=tmu0 --include_path="D:/ti/ccs700/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/include" --include_path="D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/headers/include" --include_path="D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/include" -g --define=CPU1 --define=_LAUNCHXL_F28377S --define=_FLASH --display_error_number --diag_warning=225 --diag_suppress=1311 --preproc_with_compile --preproc_dependency="adc_soc_continuous_dma_cpu01.d" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ adc_soc_continuous_dma_cpu01/CPU1_RAM/subdir_vars.mk
@@ -0,0 +1,94 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+ASM_SRCS += \
+D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_CodeStartBranch.asm \
+D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_usDelay.asm 
+
+C_SRCS += \
+D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_Adc.c \
+D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_DefaultISR.c \
+D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_Dma.c \
+D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/headers/source/F2837xS_GlobalVariableDefs.c \
+D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_Gpio.c \
+D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_PieCtrl.c \
+D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_PieVect.c \
+D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_SysCtrl.c \
+D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/examples/cpu1/adc_soc_continuous_dma/cpu01/adc_soc_continuous_dma_cpu01.c 
+
+C_DEPS += \
+./F2837xS_Adc.d \
+./F2837xS_DefaultISR.d \
+./F2837xS_Dma.d \
+./F2837xS_GlobalVariableDefs.d \
+./F2837xS_Gpio.d \
+./F2837xS_PieCtrl.d \
+./F2837xS_PieVect.d \
+./F2837xS_SysCtrl.d \
+./adc_soc_continuous_dma_cpu01.d 
+
+OBJS += \
+./F2837xS_Adc.obj \
+./F2837xS_CodeStartBranch.obj \
+./F2837xS_DefaultISR.obj \
+./F2837xS_Dma.obj \
+./F2837xS_GlobalVariableDefs.obj \
+./F2837xS_Gpio.obj \
+./F2837xS_PieCtrl.obj \
+./F2837xS_PieVect.obj \
+./F2837xS_SysCtrl.obj \
+./F2837xS_usDelay.obj \
+./adc_soc_continuous_dma_cpu01.obj 
+
+ASM_DEPS += \
+./F2837xS_CodeStartBranch.d \
+./F2837xS_usDelay.d 
+
+OBJS__QUOTED += \
+"F2837xS_Adc.obj" \
+"F2837xS_CodeStartBranch.obj" \
+"F2837xS_DefaultISR.obj" \
+"F2837xS_Dma.obj" \
+"F2837xS_GlobalVariableDefs.obj" \
+"F2837xS_Gpio.obj" \
+"F2837xS_PieCtrl.obj" \
+"F2837xS_PieVect.obj" \
+"F2837xS_SysCtrl.obj" \
+"F2837xS_usDelay.obj" \
+"adc_soc_continuous_dma_cpu01.obj" 
+
+C_DEPS__QUOTED += \
+"F2837xS_Adc.d" \
+"F2837xS_DefaultISR.d" \
+"F2837xS_Dma.d" \
+"F2837xS_GlobalVariableDefs.d" \
+"F2837xS_Gpio.d" \
+"F2837xS_PieCtrl.d" \
+"F2837xS_PieVect.d" \
+"F2837xS_SysCtrl.d" \
+"adc_soc_continuous_dma_cpu01.d" 
+
+ASM_DEPS__QUOTED += \
+"F2837xS_CodeStartBranch.d" \
+"F2837xS_usDelay.d" 
+
+C_SRCS__QUOTED += \
+"D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_Adc.c" \
+"D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_DefaultISR.c" \
+"D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_Dma.c" \
+"D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/headers/source/F2837xS_GlobalVariableDefs.c" \
+"D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_Gpio.c" \
+"D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_PieCtrl.c" \
+"D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_PieVect.c" \
+"D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_SysCtrl.c" \
+"D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/examples/cpu1/adc_soc_continuous_dma/cpu01/adc_soc_continuous_dma_cpu01.c" 
+
+ASM_SRCS__QUOTED += \
+"D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_CodeStartBranch.asm" \
+"D:/ti/c2000/C2000Ware_1_00_04_00/device_support/f2837xs/common/source/F2837xS_usDelay.asm" 
+
+
